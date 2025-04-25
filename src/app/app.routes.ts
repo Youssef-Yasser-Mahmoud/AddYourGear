c091240d475e26223c9b3b040ccdfcdc9c499dc8
Youssef <youssefyaser2020@gmail.com>
2025-04-26 02:46:38 +0300

    Changed AboutComponent to LazyLoading Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
+// import { AboutComponent } from './components/about/about.component';
 import { ProductComponent } from './components/product/product.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/about/about.component').then(
+        (p) => p.AboutComponent
+      ),
+  },
   { path: 'products', component: ProductComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
