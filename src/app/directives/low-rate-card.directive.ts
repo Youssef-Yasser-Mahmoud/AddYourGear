ea191b6a8a1ee8ba3202ef39aa17793f1e3b6966
Youssef <youssefyaser2020@gmail.com>
2025-04-26 04:10:31 +0300

    Changed it to less than 5

--- src/app/directives/low-rate-card.directive.ts
+++ src/app/directives/low-rate-card.directive.ts
@@ -9,7 +9,7 @@ export class LowRateCardDirective implements OnInit {
   constructor(private element: ElementRef) {}
 
   ngOnInit(): void {
-    if (this.rate < 4) {
+    if (this.rate < 5) {
       this.element.nativeElement.style.backgroundColor = '#ff6467';
       this.element.nativeElement.style.border = '1px solid rgb(138 60 0)';
       this.element.nativeElement.style.boxShadow = '2px 3px 9px 3px rgb(165 67 54)'
